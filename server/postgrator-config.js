1da5f1cf58a98e90f2f4a9f1436589d2c51d2337
Dominic Faulring <domcodesjs@gmail.com>
2020-10-30 12:10:02 -0700

    changed file locations

--- server/postgrator-config.js
+++
@@ -1,8 +0,0 @@
-const { DATABASE_URL, TEST_DATABASE_URL } = require('./config');
-
-module.exports = {
-  migrationDirectory: 'migrations',
-  driver: 'pg',
-  connectionString:
-    process.env.NODE_ENV === 'test' ? TEST_DATABASE_URL : DATABASE_URL
-};
